8d6ee8cf355a2cf2e2dd8edc87602388da42b706
雪霁 <wang.haiguang@puscene.com>
2018-03-14 15:28:19 +0800

    .

--- qsort.cpp
+++ qsort.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     int small[] = {3, 2, 1, 5, 4, 6};
-    int all0[10] = {0};
-    int large[100000] = {0};
+    int all0[20] = {0};
+    int large[1000] = {0};
     Utils::fillArray(all0, sizeof(all0) / sizeof(int));
-    QSort::run(small, sizeof(small) / sizeof(int));
-    Utils::log(small, sizeof(small) / sizeof(int));
+    QSort::run(all0, sizeof(all0) / sizeof(int));
+    Utils::log(all0, sizeof(all0) / sizeof(int));
     return 0;
 }
--- qsort.h
+++ qsort.h
@@ -3,34 +3,44 @@
 using namespace std;
 
 namespace QSort {
+    struct Offsets {
+        int left;
+        int right;
+    };
+
     void randomize(int arr[], int size) {
         int rindex = rand() % size;
         swap(arr[rindex], arr[size - 1]);
     }
 
-    int partition(int arr[], int size) {
+    Offsets partition(int arr[], int size) {
         randomize(arr, size);
         int left = 0,
             right = size - 1,
-            pivot = right;
+            pivot = arr[right];
 
         for (int i = 0; i < right; i++) {
-            if (arr[i] <= arr[pivot]) {
+            if (arr[i] == pivot) swap(arr[i], arr[--right]);
+
+            if (arr[i] > pivot) {
                 if (i != left) swap(arr[i], arr[left]);
                 left++;
             }
         }
 
-        swap(arr[left], arr[pivot]);
+        for (int i = left, j = size - 1; i < right && j >= right; i++, j--) {
+            swap(arr[i], arr[j]);
+        }
 
-        return left;
+        Offsets offsets = {left, left + size - right};
+        return offsets;
     }
 
     void divide(int arr[], int size) {
         if (size <= 1) return;
-        int offset = partition(arr, size);
-        divide(arr, offset);
-        divide(arr + offset + 1, size - offset - 1);
+        Offsets offsets = partition(arr, size);
+        divide(arr, offsets.left);
+        divide(arr + offsets.right, size - offsets.right);
     }
 
     void run(int a[], int size) {
